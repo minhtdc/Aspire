0002897da534ee171e8c81e5312935622861d8b2
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-23 11:10:42 +0700

    Chuyển màn hình

--- app/src/main/java/com/example/aspire/LoginActivity.java
+++ app/src/main/java/com/example/aspire/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example.aspire;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,5 +18,31 @@ public class LoginActivity extends AppCompatActivity {
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     setContentView(R.layout.login_layout);
 
+    //get view frm layout
+    Button btnLogin;
+
+    //button quay lại trên title
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+
+    btnLogin = findViewById(R.id.btn_login);
+
+    btnLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), NewFeedActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+      }
+    });
+
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Intent intent = new Intent(getApplicationContext(), StartingActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+    startActivity(intent);
+    return super.onOptionsItemSelected(item);
   }
 }
