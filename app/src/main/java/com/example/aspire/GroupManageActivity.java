a6c9d7922d2ee676a800864f4a631f51c76c8a83
levanbi12012000 <bicongnghe@gmail.com>
2020-07-23 14:15:20 +0700

    layout adapter
    mới sửa lại nha

--- app/src/main/java/com/example/aspire/GroupManageActivity.java
+++ app/src/main/java/com/example/aspire/GroupManageActivity.java
@@ -1,15 +1,42 @@
 package com.example.aspire;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.aspire.adapter.AdapterMemberManage;
+import com.example.aspire.data_models.MemberManage;
+
+import java.util.ArrayList;
+
 public class GroupManageActivity extends AppCompatActivity {
+        private AdapterMemberManage adapter;
+        private ArrayList<MemberManage> listMember;
+        ListView listGroupManage;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.group_manager_layout);
+            setTitle("Danh sách nhóm");
+            listGroupManage = findViewById(R.id.listGroup);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.group_manager_layout);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            // get view from layout
+            ImageView imgUser = (ImageView) findViewById(R.id.userAvatar);
+            // Button btnXoa = (Button) findViewById(R.id.btnXoa);
+            EditText edtUserName = (EditText) findViewById(R.id.edtName);
 
+            listMember = new ArrayList<MemberManage>();
+
+            MemberManage memberManage1 = new MemberManage("avatar nhom 1","hihi");
+            listMember.add(memberManage1);
+            MemberManage memberManage2 = new MemberManage("avatar nhom2","liuliu");
+            listMember.add(memberManage2);
+            adapter = new AdapterMemberManage(this, R.layout.listview_members_manage_layout, listMember);
+            listGroupManage.setAdapter(adapter);
     }
 }
