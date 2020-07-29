a12a0dd49f733c563bd6f23f448f1af529990b48
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-29 14:27:54 +0700

    db

--- app/src/main/java/com/example/aspire/StartingActivity.java
+++ app/src/main/java/com/example/aspire/StartingActivity.java
@@ -18,14 +18,14 @@ public class StartingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.starting_layout);
         //Check user logged
-        Users user = new Users();
-        if (user.isLogged()) {
-            Intent intent = new Intent(getApplicationContext(), NewFeedActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            startActivity(intent);
-           Toast.makeText(this, "Welcome you comeback", Toast.LENGTH_SHORT).show();
-            finish();
-        }
+//        Users user = new Users();
+//        if (user.isLogged()) {
+//            Intent intent = new Intent(getApplicationContext(), NewFeedActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//            startActivity(intent);
+//           Toast.makeText(this, "Welcome you comeback", Toast.LENGTH_SHORT).show();
+//            finish();
+//        }
 
         Button btn_login = findViewById(R.id.btn_login);
         Button btn_sign = findViewById(R.id.btn_signUp);
