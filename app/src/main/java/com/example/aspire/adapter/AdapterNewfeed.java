e0d989ca3c26e64542cb40e01ab32cab73cabd9c
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-29 14:40:08 +0700

    update giao diện

--- app/src/main/java/com/example/aspire/adapter/AdapterNewfeed.java
+++ app/src/main/java/com/example/aspire/adapter/AdapterNewfeed.java
@@ -32,7 +32,6 @@ public class AdapterNewfeed extends ArrayAdapter<Newfeed> {
 
     //define view holder
     static class ViewHolder {
-        ImageView imgUser;
         TextView txtName;
         TextView txtPeople;
     }
@@ -47,7 +46,6 @@ public class AdapterNewfeed extends ArrayAdapter<Newfeed> {
             convertView = context.getLayoutInflater().inflate(layoutID, parent, false);
 
 
-            viewHolder.imgUser =(ImageView) convertView.findViewById(R.id.imgHinhAnh);
             viewHolder.txtName = (TextView) convertView.findViewById(R.id.txtuserName);
             viewHolder.txtPeople = (TextView) convertView.findViewById(R.id.txtPeople);
 
