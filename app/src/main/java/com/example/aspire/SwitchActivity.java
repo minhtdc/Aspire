0d94d7a587516ed14c08e33029acb0eedd6ebc6a
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-07-29 14:44:24 +0700

    update

--- app/src/main/java/com/example/aspire/SwitchActivity.java
+++ app/src/main/java/com/example/aspire/SwitchActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 public class SwitchActivity {
     public static void goToNewFeed(final Context context) {
         //Starting a new Intent
-        Intent intent = new Intent(context, NewFeedActivity.class);
+        Intent intent = new Intent(context, CreateGroupActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         context.startActivity(intent);
     }
