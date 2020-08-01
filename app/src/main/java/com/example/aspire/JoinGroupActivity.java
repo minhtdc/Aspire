58105bb5347b2cbd988fa4cd9fbc7e88a85fbf8f
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-08-01 16:06:18 +0700

    sc

--- app/src/main/java/com/example/aspire/JoinGroupActivity.java
+++ app/src/main/java/com/example/aspire/JoinGroupActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.aspire.data_models.Groups;
+import com.example.aspire.data_models.Requests;
 import com.example.aspire.data_models.Users;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,36 +20,54 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+
 public class JoinGroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         final TextView txtGroupInfo;
-
+        final EditText edtGroupJoin;
+        final Button btnJoin, btnCancel;
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_group_layout);
         txtGroupInfo = findViewById(R.id.txtGroupInfo);
+        edtGroupJoin = findViewById(R.id.edtGroupJoin);
+        btnJoin = findViewById(R.id.btnJoin);
+        btnCancel = findViewById(R.id.btnCancel);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        Users users = new Users();
-        txtGroupInfo.setText(users.getUserName());
+        final Requests request = new Requests();
 
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.addValueEventListener(new ValueEventListener() {
+        txtGroupInfo.setText("Group info");
+        btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
-                //Toast.makeText(JoinGroupActivity.this, value, Toast.LENGTH_SHORT).show();
-                //txtGroupInfo.setText(value);
-            }
+            public void onClick(View v) {
+                if(btnJoin.getText().toString() == "Hủy yêu cầu") {
+                    DatabaseReference remove = FirebaseDatabase.getInstance().getReference("requests").child(request.getGroupID());
+                    remove.removeValue();
+                    Toast.makeText(JoinGroupActivity.this, "Hủy yêu cầu thành công", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                    btnJoin.setText("Tham gia");
+                }
+                //
+                else{
+                    request.setGroupID("-MDO_NZb1_UPUjrhmBq3");
+                    request.setAdminID("zoLTQiwpsxgLjXkUFrvr5v5y9Kx1");
+                    request.setContent(edtGroupJoin.getText().toString());
+                    request.setMemberID("TavlafWDc5SsUdv0jsYjsA7g5nX2");
+                        try {
+                            request.addRequestsToDatabase(request);
+                            Toast.makeText(JoinGroupActivity.this, "Gửi yêu càu thành công", Toast.LENGTH_SHORT).show();
+                            btnJoin.setText("Hủy yêu cầu");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                }
 
             }
         });
--- app/src/main/java/com/example/aspire/data_models/Groups.java
+++ app/src/main/java/com/example/aspire/data_models/Groups.java
@@ -6,6 +6,7 @@ import com.example.aspire.android_2_func;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,11 +23,10 @@ public class Groups {
         listIDMember = new ArrayList<String>();
     }
 
-    public Groups(String groupID, String adminId, String groupName, DatabaseReference mDatabase, String groupInfo, ArrayList<String> listIDMember) {
+    public Groups(String groupID, String adminId, String groupName, String groupInfo, ArrayList<String> listIDMember) {
         this.groupID = groupID;
         this.adminId = adminId;
         this.groupName = groupName;
-        this.mDatabase = mDatabase;
         this.groupInfo = groupInfo;
         this.listIDMember = new ArrayList<String>();
     }
@@ -81,7 +81,8 @@ public class Groups {
         this.listIDMember = listIDMember;
     }
 
-    //ham ađ group
+
+    //
     public void addGroupToDatabase(Groups group) throws JSONException {
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
         Map<String, Object> postValues = null;
@@ -98,6 +99,22 @@ public class Groups {
         mDatabase.updateChildren(childUpdates);
     }
 
+//    private void SeachGroupInformation(String GroupID, String userNameID, Newfeed newfeed) throws JSONException {
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        Map<String, Object> postValues = null;
+//        Log.d("JSON Group: ", newfeed.toJSON(newfeed).toString());
+//
+//        try {
+//            postValues = android_2_func.toMap(newfeed.toJSON(newfeed));
+//        } catch (JSONException e) {
+//        }
+//
+//        Map<String, Object> childUpdates = new HashMap<>();
+//        childUpdates.put("/groups/" + GroupID + userNameID, postValues);
+//
+//        mDatabase.updateChildren(childUpdates);
+//    }
+
     //
     public JSONObject toJSON(Groups group) throws JSONException {
         JSONObject json = new JSONObject();
@@ -106,6 +123,7 @@ public class Groups {
         json.put("admin_id", group.getAdminId());
         return json;
     }
-
 }
+
+
 
