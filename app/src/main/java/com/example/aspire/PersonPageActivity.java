6e9f3f3c6241f37095de7ef686724ebf2e55c075
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-08-03 18:06:03 +0700

    logout

--- app/src/main/java/com/example/aspire/PersonPageActivity.java
+++ app/src/main/java/com/example/aspire/PersonPageActivity.java
@@ -6,13 +6,34 @@ import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PersonPageActivity extends AppCompatActivity {
     Button btnGroupManager;
     Button btnYourGroup;
     Button btnSetting;
+    Button btnLogOut;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener(){
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            if (firebaseAuth.getCurrentUser() == null){
+                //Do anything here which needs to be done after signout is complete
+                Intent intent = new Intent(PersonPageActivity.this,LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+            else {
+
+            }
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +42,19 @@ public class PersonPageActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        btnLogOut = findViewById(R.id.btnLogOut);
         btnGroupManager = findViewById(R.id.btnManageGroup);
         btnYourGroup = findViewById(R.id.btnYourGroup);
         btnSetting = findViewById(R.id.btnSetting);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.addAuthStateListener(authStateListener);
+                auth.signOut();
+                finish();
+            }
+        });
 
         btnGroupManager.setOnClickListener(new View.OnClickListener() {
             @Override
