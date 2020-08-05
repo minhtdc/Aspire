1747218cdd12ece2fe126eea8bff46e0593b5851
minhtdc <67999471+minhtdc@users.noreply.github.com>
2020-08-05 13:24:09 +0700

    al

--- app/src/main/java/com/example/aspire/GroupManageActivity.java
+++ app/src/main/java/com/example/aspire/GroupManageActivity.java
@@ -4,91 +4,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.aspire.adapter.AdapterNewfeed;
-import com.example.aspire.data_models.Groups;
-import com.example.aspire.data_models.Users;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.example.aspire.adapter.AdapterMemberManage;
+import com.example.aspire.data_models.MemberManage;
 
 import java.util.ArrayList;
 
 public class GroupManageActivity extends AppCompatActivity {
-    ListView listViewGroup;
-    private ArrayList<Groups> listGroup;
+
     Button addGroup;
-    private AdapterNewfeed adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.group_manager_layout);
-        setTitle("Nhóm bạn quản lí");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.group_manager_layout);
+            setTitle("Nhóm bạn quản lí");
 
-        addGroup = findViewById(R.id.btnAddGroup);
-        listViewGroup = findViewById(R.id.listGroupManage);
-        listGroup = new ArrayList<Groups>();
+            addGroup = findViewById(R.id.btnAddGroup);
 
-        adapter = new AdapterNewfeed(this, R.layout.listview_newfeed_layout, listGroup);
-        listViewGroup.setAdapter(adapter);
 
-        final Users user = new Users(); // để lấu userid so sánh
-        // đưa dữ liệu từ db lên listview
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myGroups = database.getReference("groups");
-        myGroups.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull final DataSnapshot snapshot1) {
-                adapter.clear();
-                for (final DataSnapshot data : snapshot1.getChildren()) {
-                    String idGroup = data.getKey();
-                    DatabaseReference getAdminID = FirebaseDatabase.getInstance().getReference("groups").child(idGroup).child("adminID");
-                    //lấy id admin của các nhóm ra
-                    getAdminID.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot2) {
-                            String adminID = snapshot2.getValue(String.class);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
 
-                            //nếu adminId của nhóm bằng với user thì hiển thị lên list view
-                            if(adminID.equals(user.getUserID())){
-                                Groups group = data.getValue(Groups.class);
-                                adapter.add(group);
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
+            addGroup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(GroupManageActivity.this, CreateGroupActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(intent);
                 }
+            });
 
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        addGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GroupManageActivity.this, CreateGroupActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(intent);
-            }
-        });
 
     }
 }
--- app/src/main/java/com/example/aspire/data_models/Comments.java
+++ app/src/main/java/com/example/aspire/data_models/Comments.java
@@ -1,26 +1,11 @@
 package com.example.aspire.data_models;
 
-import android.util.Log;
-
 import com.example.aspire.R;
-import com.example.aspire.android_2_func;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Comments {
     private int userAvatar;
     private String userName;
     private String userComment;
-    private ArrayList<Comments> listComments;
-    FirebaseDatabase database;
-    DatabaseReference mDatabase;
 
     public Comments(String userName, String userComment) {
         this.userAvatar = R.drawable.anhdaidien;
@@ -52,27 +37,4 @@ public class Comments {
         this.userComment = userComment;
     }
 
-    public JSONObject toJSON(Comments comment) throws JSONException {
-        JSONObject json = new JSONObject();
-        json.put("userName", comment.getUserName());
-        json.put("userComment", comment.getUserComment());
-        json.put("userAvatar", Character.toString(comment.getUserName().toUpperCase().charAt(0)));
-        return json;
-    }
-
-    public void addComment(String idComment, String idGroup, String idPost, Comments comment) throws JSONException {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        Map<String, Object> postValues = null;
-        Log.d("JSON User: ", comment.toJSON(comment).toString());
-
-        try {
-            postValues = android_2_func.toMap(comment.toJSON(comment));
-        } catch (JSONException e) {
-        }
-
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put("/groups/" + idGroup + "/posts/" + idPost + "/comments/" + idComment + "/", postValues);
-
-        mDatabase.updateChildren(childUpdates);
-    }
 }
